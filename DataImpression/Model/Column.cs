073585c3baf9feae065e6c03a2b5513cad09d691
lvovandrey <lvovandrey@mail.ru>
2020-03-02 22:52:33 +0300

    Добавил в модель основные классы, участвующие во вводе данных.

---
+++ DataImpression/Model/Column.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImpression.Model
+{
+    /// <summary>
+    /// Столбец
+    /// </summary>
+    class Column
+    {
+        #region ctor
+        public Column(int orderedNumber, string name)
+        {
+            OrderedNumber = orderedNumber;
+            Name = name;
+        }
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Порядковый номер столбца
+        /// </summary>
+        int OrderedNumber { get; set; }
+
+        /// <summary>
+        /// Название столбца
+        /// </summary>
+        string Name { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+    }
+}
---
+++ DataImpression/Model/TOI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImpression.Model
+{
+    /// <summary>
+    /// Временная зона - отрезок времени из полного временного интервала.
+    /// Под этим можно понимать например часть времени, которая анализируется из всей записи.
+    /// </summary>
+    class TOI
+    {
+        #region ctor
+        public TOI(TimeInterval workingTimeInterval, TimeInterval fullTimeInterval)
+        {
+            WorkingTimeInterval = workingTimeInterval;//TODO: Валидация данных - если workingTimeInterval вне fullTimeInterval - кинуть Exception
+            FullTimeInterval = fullTimeInterval;
+        }
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Рабочий интервал времени - т.е. тот с которым непосредственно будет производиться работа, например, на нем будут анализироваться данные
+        /// </summary>
+        TimeInterval WorkingTimeInterval { get; set; }
+
+        /// <summary>
+        /// Полный интервал времени - т.е. вообще весь временной отрезок, внутри которого находится
+        /// </summary>
+        TimeInterval FullTimeInterval { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+
+    }
+}
