1d26206d4cbf49366cb0a03640035fc468efaa3d
lvovandrey <lvovandrey@mail.ru>
2020-07-11 23:49:33 +0300

    разные изменения, связанные с обработкой

---
+++ DataImpression/Models/ProcessingResults.cs
@@ -0,0 +1,45 @@
+﻿using DataImpression.Models.ResultTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImpression.Models
+{
+    class ProcessingResults
+    {
+        #region ctor
+        public ProcessingResults()
+        {
+        }
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Распределение суммарного относительног времени фиксаций (в виде доли от полного времени) по функциональным зонам
+        /// т.е. если value = 0.5 для данной faoi, значит туда смотрели половину от всего времени
+        /// </summary>
+        public FAOIDistributed_Parameter<double> TimePercentDistribution { get; set; }
+        #endregion
+
+        #region Methods
+        #endregion
+
+        #region Fields
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+
+
+        #endregion
+
+
+    }
+}
---
+++ DataImpression/Models/ResultTypes/FAOIDistributed_Parameter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImpression.Models.ResultTypes
+{
+    public class FAOIDistributed_Parameter<T>
+    {
+        public string ParameterName;
+        public List<FAOI_Value_Pair<T>> Results = new List<FAOI_Value_Pair<T>>();
+
+        public FAOIDistributed_Parameter(string parameterName)
+        {
+            ParameterName = parameterName;
+        }
+    }
+
+    /// <summary>
+    /// Пара "зона-значение параметра"
+    /// </summary>
+    public class FAOI_Value_Pair<T>
+    {
+        public T Value { get; set; }
+        public FAOI FAOI { get; set; }
+    }
+}
