8413cfc817405b777cdcae2c4dbd921225a044e8
lvovandrey <lvovandrey@mail.ru>
2020-09-17 13:15:23 +0300

    Начал переносить потихоньку из MainWindowVM в CSVOpenMasterVM код

---
+++ DataImpression/ViewModel/CSVOpenMasterVM.cs
@@ -0,0 +1,168 @@
+﻿using DataImpression.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataImpression.ViewModel
+{
+    public enum CSVFileOpenStage
+    {
+        None,
+        TimeColumnChoice,
+        AOIHitColumnsChoice,
+        FAOIsInput,
+        ProcessingTask,
+        ViewResults
+    }
+
+
+    public class CSVOpenMasterVM
+    {
+        #region ctor
+        public CSVOpenMasterVM(Model _model, MainWindow mainWindow)
+        {
+            TimeColumnChoiceVM = new TimeColumnChoiceVM(_model);
+            AOIHitColumnsChoiceVM = new AOIHitColumnsChoiceVM(_model);
+            FAOIsInputVM = new FAOIsInputVM(_model, MainWindow.FAOIsInput.FAOIsInputListView);
+            ProcessingTaskVM = new ProcessingTaskVM(_model);
+
+            InputStage = CSVFileOpenStage.None;
+        }
+        #endregion
+
+        #region Fields
+        /// <summary>
+        /// Модель данных
+        /// </summary>
+        Model _model;
+
+        MainWindow MainWindow;
+        #endregion
+
+
+        #region Properties
+        TimeColumnChoiceVM timeColumnChoiceVM;
+        public TimeColumnChoiceVM TimeColumnChoiceVM { get { return timeColumnChoiceVM; } set { timeColumnChoiceVM = value; OnPropertyChanged("TimeColumnChoiceVM"); } }
+
+        AOIHitColumnsChoiceVM aOIHitColumnsChoiceVM;
+        public AOIHitColumnsChoiceVM AOIHitColumnsChoiceVM { get { return aOIHitColumnsChoiceVM; } set { aOIHitColumnsChoiceVM = value; OnPropertyChanged("AOIHitColumnsChoiceVM"); } }
+
+        FAOIsInputVM fAOIsInputVM;
+        public FAOIsInputVM FAOIsInputVM { get { return fAOIsInputVM; } set { fAOIsInputVM = value; OnPropertyChanged("FAOIsInputVM"); } }
+
+        ProcessingTaskVM processingTaskVM;
+        public ProcessingTaskVM ProcessingTaskVM { get { return processingTaskVM; } set { processingTaskVM = value; OnPropertyChanged("ProcessingTaskVM"); } }
+
+
+        private CSVFileOpenStage inputStage;
+        public CSVFileOpenStage InputStage
+        {
+            get
+            {
+                return inputStage;
+            }
+            set
+            {
+                inputStage = value;
+                OnPropertyChanged("InputStage");
+                OnPropertyChanged("InputPageTitle");
+                RefreshInputElementsVisibility();
+            }
+        }
+
+        bool canExecuteNextInputStage = false;
+        bool CanExecuteNextInputStage
+        {
+            get
+            {
+                switch (inputStage)
+                {
+                    case CSVFileOpenStage.None:
+                        {
+                            return false;
+                            break;
+                        }
+                    case CSVFileOpenStage.TimeColumnChoice:
+                        {
+                            return TimeColumnChoiceVM.CanExecuteNextInputStage();
+                            break;
+                        }
+                    case CSVFileOpenStage.AOIHitColumnsChoice:
+                        {
+                            return AOIHitColumnsChoiceVM.CanExecuteNextInputStage();
+                            break;
+                        }
+                    case CSVFileOpenStage.FAOIsInput:
+                        {
+                            return FAOIsInputVM.CanExecuteNextInputStage();
+                            break;
+                        }
+                    case CSVFileOpenStage.ProcessingTask:
+                        {
+                            return ProcessingTaskVM.CanExecuteNextInputStage();
+                            break;
+                        }
+                    case CSVFileOpenStage.ViewResults:
+                        {
+                            return ResultsViewAreaVM.CanExecuteNextInputStage();
+                            break;
+                        }
+                    default:
+                        {
+                            return false;
+                            break;
+                        }
+                }
+            }
+        }
+
+        public string InputPageTitle
+        {
+            get
+            {
+                switch (inputStage)
+                {
+                    case CSVFileOpenStage.None:
+                        {
+                            return "";
+                            break;
+                        }
+                    case CSVFileOpenStage.TimeColumnChoice:
+                        {
+                            return "Выбор колонки csv-файла с временем";
+                            break;
+                        }
+                    case CSVFileOpenStage.AOIHitColumnsChoice:
+                        {
+                            return "Выбор колонки csv-файла с попаданиями маркера взгляда в размеченные зоны (AOI Hit)";
+                            break;
+                        }
+                    case CSVFileOpenStage.FAOIsInput:
+                        {
+                            return "Ввод перечня функциональных зон (FAOI)";
+                            break;
+                        }
+                    case CSVFileOpenStage.ProcessingTask:
+                        {
+                            return "Обработка введенных данных, построение внутренней модели";
+                            break;
+                        }
+                    case CSVFileOpenStage.ViewResults:
+                        {
+                            return "Отображение результатов";
+                            break;
+                        }
+                    default:
+                        {
+                            return "";
+                            break;
+                        }
+                }
+            }
+        }
+
+        #endregion
+    }
+}
--- DataImpression/ViewModel/MainWindowViewModel.cs
+++ DataImpression/ViewModel/MainWindowViewModel.cs
@@ -6,15 +6,7 @@ using System.Windows;
 
 namespace DataImpression.ViewModel
 {
-    public enum InputStage
-    {
-        None,
-        TimeColumnChoice,
-        AOIHitColumnsChoice,
-        FAOIsInput,
-        ProcessingTask,
-        ViewResults
-    }
+
     public class MainWindowViewModel : INPCBase
     {
 
@@ -23,14 +15,11 @@ namespace DataImpression.ViewModel
         {
             _model = model;
             MainWindow = mainWindow;
-            TimeColumnChoiceVM = new TimeColumnChoiceVM(_model);
-            AOIHitColumnsChoiceVM = new AOIHitColumnsChoiceVM(_model);
-            FAOIsInputVM = new FAOIsInputVM(_model, MainWindow.FAOIsInput.FAOIsInputListView);
-            ProcessingTaskVM = new ProcessingTaskVM(_model);
+
             //  FAOIDiagramVM = new FAOIDiagramVM(_model);
             ResultsViewAreaVM = new ResultsViewAreaVM(_model, this);
-            InputStage = InputStage.None;
-            OnPropertyChanged("TimeColumnChoiceOpacity");
+          
+
         }
         #endregion
 
@@ -45,131 +34,12 @@ namespace DataImpression.ViewModel
 
 
         #region Properties
-        TimeColumnChoiceVM timeColumnChoiceVM;
-        public TimeColumnChoiceVM TimeColumnChoiceVM { get { return timeColumnChoiceVM; } set { timeColumnChoiceVM = value; OnPropertyChanged("TimeColumnChoiceVM"); } }
-
-        AOIHitColumnsChoiceVM aOIHitColumnsChoiceVM;
-        public AOIHitColumnsChoiceVM AOIHitColumnsChoiceVM { get { return aOIHitColumnsChoiceVM; } set { aOIHitColumnsChoiceVM = value; OnPropertyChanged("AOIHitColumnsChoiceVM"); } }
-
-        FAOIsInputVM fAOIsInputVM;
-        public FAOIsInputVM FAOIsInputVM { get { return fAOIsInputVM; } set { fAOIsInputVM = value; OnPropertyChanged("FAOIsInputVM"); } }
-
-        ProcessingTaskVM processingTaskVM;
-        public ProcessingTaskVM ProcessingTaskVM { get { return processingTaskVM; } set { processingTaskVM = value; OnPropertyChanged("ProcessingTaskVM"); } }
-
-        //FAOIDiagramVM fAOIDiagramVM;
-        //public FAOIDiagramVM FAOIDiagramVM { get { return fAOIDiagramVM; } set { fAOIDiagramVM = value; OnPropertyChanged("FAOIDiagramVM"); } }
-
+        
         ResultsViewAreaVM resultsViewAreaVM;
         public ResultsViewAreaVM ResultsViewAreaVM { get { return resultsViewAreaVM; } set { resultsViewAreaVM = value; OnPropertyChanged("ResultsViewAreaVM"); } }
 
 
-        private InputStage inputStage;
-        public InputStage InputStage
-        {
-            get
-            {
-                return inputStage;
-            }
-            set
-            {
-                inputStage = value;
-                OnPropertyChanged("InputStage");
-                OnPropertyChanged("InputPageTitle");
-                RefreshInputElementsVisibility();
-            }
-        }
 
-        bool canExecuteNextInputStage = false;
-        bool CanExecuteNextInputStage
-        {
-            get
-            {
-                switch (inputStage)
-                {
-                    case InputStage.None:
-                        {
-                            return false;
-                            break;
-                        }
-                    case InputStage.TimeColumnChoice:
-                        {
-                            return TimeColumnChoiceVM.CanExecuteNextInputStage();
-                            break;
-                        }
-                    case InputStage.AOIHitColumnsChoice:
-                        {
-                            return AOIHitColumnsChoiceVM.CanExecuteNextInputStage();
-                            break;
-                        }
-                    case InputStage.FAOIsInput:
-                        {
-                            return FAOIsInputVM.CanExecuteNextInputStage();
-                            break;
-                        }
-                    case InputStage.ProcessingTask:
-                        {
-                            return ProcessingTaskVM.CanExecuteNextInputStage();
-                            break;
-                        }
-                    case InputStage.ViewResults:
-                        {
-                            return ResultsViewAreaVM.CanExecuteNextInputStage();
-                            break;
-                        }
-                    default:
-                        {
-                            return false;
-                            break;
-                        }
-                }
-            }
-        }
-
-        public string InputPageTitle
-        {
-            get
-            {
-                switch (inputStage)
-                {
-                    case InputStage.None:
-                        {
-                            return "";
-                            break;
-                        }
-                    case InputStage.TimeColumnChoice:
-                        {
-                            return "Выбор колонки csv-файла с временем";
-                            break;
-                        }
-                    case InputStage.AOIHitColumnsChoice:
-                        {
-                            return "Выбор колонки csv-файла с попаданиями маркера взгляда в размеченные зоны (AOI Hit)";
-                            break;
-                        }
-                    case InputStage.FAOIsInput:
-                        {
-                            return "Ввод перечня функциональных зон (FAOI)";
-                            break;
-                        }
-                    case InputStage.ProcessingTask:
-                        {
-                            return "Обработка введенных данных, построение внутренней модели";
-                            break;
-                        }
-                    case InputStage.ViewResults:
-                        {
-                            return "Отображение результатов";
-                            break;
-                        }
-                    default:
-                        {
-                            return "";
-                            break;
-                        }
-                }
-            }
-        }
         #endregion
 
         #region Methods
@@ -182,7 +52,7 @@ namespace DataImpression.ViewModel
             OpenFileDialog openFileDialog = new OpenFileDialog();
             if (openFileDialog.ShowDialog() == false) return;
 
-            InputStage = InputStage.TimeColumnChoice;
+            InputStage = CSVFileOpenStage.TimeColumnChoice;
             _model.SourceData.CSVFileName = openFileDialog.FileName;// файлнейм в модель  закидываем
             try
             {
@@ -193,7 +63,7 @@ namespace DataImpression.ViewModel
             }
             catch
             {
-                InputStage = InputStage.None;
+                InputStage = CSVFileOpenStage.None;
                 MessageBox.Show("Не удалось считать заголовок csv-файла. Попробуйте открыть файл вручную и убедиться в правильности его формата.");
             }
         }
@@ -204,43 +74,43 @@ namespace DataImpression.ViewModel
         {
             switch (inputStage)
             {
-                case InputStage.None:
+                case CSVFileOpenStage.None:
                     {
-                        InputStage = InputStage.TimeColumnChoice;
+                        InputStage = CSVFileOpenStage.TimeColumnChoice;
                         break;
                     }
-                case InputStage.TimeColumnChoice:
+                case CSVFileOpenStage.TimeColumnChoice:
                     {
-                        InputStage = InputStage.AOIHitColumnsChoice;
+                        InputStage = CSVFileOpenStage.AOIHitColumnsChoice;
                         AOIHitColumnsChoiceVM = new AOIHitColumnsChoiceVM(_model);
                         break;
                     }
-                case InputStage.AOIHitColumnsChoice:
+                case CSVFileOpenStage.AOIHitColumnsChoice:
                     {
-                        InputStage = InputStage.FAOIsInput;
+                        InputStage = CSVFileOpenStage.FAOIsInput;
                         FAOIsInputVM = new FAOIsInputVM(_model, MainWindow.FAOIsInput.FAOIsInputListView);
                         break;
                     }
-                case InputStage.FAOIsInput:
+                case CSVFileOpenStage.FAOIsInput:
                     {
                         if (!FAOIsInputVM.RecordResultsToModel()) return; //TODO: вот это тоже плохо - тут он нужен этот метод, а в других местах его нет. А если забуду?
-                        InputStage = InputStage.ProcessingTask;
+                        InputStage = CSVFileOpenStage.ProcessingTask;
                         break;
                     }
-                case InputStage.ProcessingTask:
+                case CSVFileOpenStage.ProcessingTask:
                     {
-                        InputStage = InputStage.ViewResults;
+                        InputStage = CSVFileOpenStage.ViewResults;
                         ResultsViewAreaVM = new ResultsViewAreaVM(_model, this);
                         break;
                     }
-                case InputStage.ViewResults:
+                case CSVFileOpenStage.ViewResults:
                     {
-                        InputStage = InputStage.None;
+                        InputStage = CSVFileOpenStage.None;
                         break;
                     }
                 default:
                     {
-                        InputStage = InputStage.None;
+                        InputStage = CSVFileOpenStage.None;
                         break;
                     }
             }
@@ -251,23 +121,23 @@ namespace DataImpression.ViewModel
         void RefreshInputElementsVisibility()
         {
             if (TimeColumnChoiceVM != null)
-                if (inputStage == InputStage.TimeColumnChoice) TimeColumnChoiceVM.Visibility = Visibility.Visible;
+                if (inputStage == CSVFileOpenStage.TimeColumnChoice) TimeColumnChoiceVM.Visibility = Visibility.Visible;
                 else TimeColumnChoiceVM.Visibility = Visibility.Collapsed;
 
             if (AOIHitColumnsChoiceVM != null)
-                if (inputStage == InputStage.AOIHitColumnsChoice) AOIHitColumnsChoiceVM.Visibility = Visibility.Visible;
+                if (inputStage == CSVFileOpenStage.AOIHitColumnsChoice) AOIHitColumnsChoiceVM.Visibility = Visibility.Visible;
                 else AOIHitColumnsChoiceVM.Visibility = Visibility.Collapsed;
 
             if (FAOIsInputVM != null)
-                if (inputStage == InputStage.FAOIsInput) FAOIsInputVM.Visibility = Visibility.Visible;
+                if (inputStage == CSVFileOpenStage.FAOIsInput) FAOIsInputVM.Visibility = Visibility.Visible;
                 else FAOIsInputVM.Visibility = Visibility.Collapsed;
 
             if (ProcessingTaskVM != null)
-                if (inputStage == InputStage.ProcessingTask) ProcessingTaskVM.Visibility = Visibility.Visible;
+                if (inputStage == CSVFileOpenStage.ProcessingTask) ProcessingTaskVM.Visibility = Visibility.Visible;
                 else ProcessingTaskVM.Visibility = Visibility.Collapsed;
 
             if (ResultsViewAreaVM != null)
-                if (inputStage == InputStage.ViewResults) ResultsViewAreaVM.Visibility = Visibility.Visible;
+                if (inputStage == CSVFileOpenStage.ViewResults) ResultsViewAreaVM.Visibility = Visibility.Visible;
                 else ResultsViewAreaVM.Visibility = Visibility.Collapsed;
         }
         #endregion
